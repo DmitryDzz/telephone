208f812cfc658b954a8fdce61d994312d577ff33
DmitryDzz <dzakhovds@gmail.com>
2015-10-08 23:44:01 +0300

    #4 voice call was added but not tested

--- telephone/telephone.ino
+++ telephone/telephone.ino
@@ -1,4 +1,5 @@
 #include "button.h"
+#include <GSM.h>
 
 using namespace telephone;
 
@@ -6,6 +7,7 @@ const int HANDSET_PIN = A0;
 const int ROTATE_PIN = A1;
 const int COUNTER_PIN = A2;
 const int LED_PIN = 13;
+const int AUTO_START_GSM_PIN = 8;
 
 Button buttonHandset(0, 1023);
 Button buttonRotate(0, 1023);
@@ -13,12 +15,20 @@ Button buttonCounter(0, 1023);
 
 boolean isGettingPhoneNumber;
 boolean isGettingDigit;
+boolean isTalking;
 
 int digitCounter;
 int digitValue;
 String phoneNumber;
 
 const int DIGITS_IN_PHONE_NUMBER = 11;
+char phoneNumberChars[DIGITS_IN_PHONE_NUMBER + 1];
+
+//---- gsm begin
+#define SIM_PIN_NUMBER ""
+GSM gsmAccess;
+GSMVoiceCall gsmVoiceCall;
+//---- gsm end
 
 void pickedUpHandler(telephone::ButtonState buttonState) {
   isGettingPhoneNumber = buttonState == PRESSED;
@@ -27,6 +37,10 @@ void pickedUpHandler(telephone::ButtonState buttonState) {
     digitCounter = 0;
   } else {
     isGettingDigit = false;
+    if (isTalking) {
+      gsmVoiceCall.hangCall();
+      isTalking = false;
+    }
   }
 }
 
@@ -56,7 +70,13 @@ void rotaryCounterHandler(telephone::ButtonState buttonState) {
 
 void makeCall() {
   updatePhoneNumberForRussia();
-  Serial.println("Phone number: " + phoneNumber);
+  phoneNumber.toCharArray(phoneNumberChars, DIGITS_IN_PHONE_NUMBER + 1);
+  if (gsmVoiceCall.voiceCall(phoneNumberChars)) {
+    isTalking = true;
+    while (isTalking && (gsmVoiceCall.getvoiceCallStatus() == TALKING));
+    // ПРОВЕРИТЬ: когда трубку кладут "тут" и когда кладут "там".
+    // ПРОВЕРИТЬ: когда занято.
+  }
 }
 
 void updatePhoneNumberForRussia() {
@@ -68,14 +88,30 @@ void updatePhoneNumberForRussia() {
 
 void setup() {
   Serial.begin(9600);
+
+  Serial.println();
+  Serial.println("Started");
+
   pinMode(HANDSET_PIN, INPUT);
   pinMode(ROTATE_PIN, INPUT);
   pinMode(COUNTER_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(AUTO_START_GSM_PIN, OUTPUT);
   delay(100);
+  digitalWrite(AUTO_START_GSM_PIN, HIGH);
+
   buttonHandset.setHandler(pickedUpHandler);
   buttonRotate.setHandler(rotatingHandler);
   buttonCounter.setHandler(rotaryCounterHandler);
+  
+  while (true) {
+    if (gsmAccess.begin(SIM_PIN_NUMBER) == GSM_READY) {
+      break;
+    }
+    delay(5000);
+  }
+  
+  Serial.println("Connected to network");
 }
 
 void loop() {
